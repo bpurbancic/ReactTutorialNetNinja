d86d21ec59135cf18dfe85f4be4da014f8815a5f
BPUrbancic <BPUrbancic@gmail.com>
2022-02-20 10:28:36 -0500

    Passed a function as a prop to delete an element from the array

--- src/Home.js
+++ src/Home.js
@@ -9,13 +9,18 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id != id);
+        setBlogs(newBlogs);
+    }
+
 
 
     return (
         <div className="Home">
-            <BlogList blogs={blogs} title='All Blogs' />
+            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
             {/* blogs above is a prop used to pass data to child component*/}
-            <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
+
         </div >
     );
 }
