8d1669d4436884d845c2d0792ea56cc7a795e0c5
BPUrbancic <BPUrbancic@gmail.com>
2022-02-20 07:30:14 -0500

    All updates to date

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+
+
+
+    return (
+        <div className="Home">
+            <h1>Home Page</h1>
+            {/* <div className= */}
+
+
+        </div >
+    );
+}
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>My Awesome Blog of Awesomeness</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/Create">New Blog</a>
+
+            </div>
+
+        </nav>
+
+
+    );
+}
+
+export default Navbar;
