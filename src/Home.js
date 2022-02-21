7a00ac243cb1901ef78243ee418555df4dbe539e
BPUrbancic <BPUrbancic@gmail.com>
2022-02-20 20:14:28 -0500

    Created custom hook by moving fetch to useFetch file

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,11 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
-    // useEffect takes a function as an argument which will run upon every render and re-render
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Fetch failed.');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                setError(err.message);
-                setIsLoading(false);
-            })
-    }, []);
-    // [] only runs on first render; ie, we only fetch the data once
 
     return (
         <div className="home">
