091f041007079c2760a927412c3fcc8da39e986e
BPUrbancic <BPUrbancic@gmail.com>
2022-02-28 21:25:27 -0500

    Changed tags in Navbar to <Link>

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>My Awesome Blog of Awesomeness</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/Create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/Create">New Blog</Link>
+                {/* <a href="/">Home</a>
+                <a href="/Create">New Blog</a> */}
+
 
             </div>
 
