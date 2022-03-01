96ff732fd6cc18b3ab22435e664370992b3eaa3e
BPUrbancic <BPUrbancic@gmail.com>
2022-02-28 21:53:08 -0500

    Added AbortController to stop fetch if new page is requested mid-fetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,23 +4,37 @@ const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Fetch failed.');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setData(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                setError(err.message);
-                setIsLoading(false);
-            })
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('Fetch failed.');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    if (err.name === "AbortError") {
+                        console.log("Fetch aborted.")
+                    } else {
+                        setError(err.message);
+                        setIsLoading(false);
+                    }
+                })
+        }, 1000);
+
+        return () => {
+            abortCont.abort();
+            console.log("cleanup");
+        };
     }, [url]);
     return { data, isLoading, error };
 }
